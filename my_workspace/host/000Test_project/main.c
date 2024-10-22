edacdee6bbc87a833ade61442455d98d288365fe
Sukhdeep-Singh-Soni <sukhdeepsinghsoni1998@gmail.com>
2024-10-22 22:23:10 +0530

    added workspaces

---
+++ my_workspace/host/000Test_project/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 22, 2024
+ *      Author: deep
+ */
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello world\n");
+
+	printf("Press any key to exit...");
+
+	getchar();
+
+	return 0;
+}
+
